39102eaedf4b6ebd3a4ec44a2b57e986fd3aaa91
MZI_ASUS\user <umutissi@gmail.com>
2018-06-29 16:30:31 +0300

    Big push

---
+++ Cycling/Assets/lase.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class lase : MonoBehaviour
+{
+    public GameObject yarat;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Instantiate(yarat, this.transform.position, this.transform.rotation);
+    }
+}
---
+++ Cycling/Assets/yaratatacak.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yaratatacak : MonoBehaviour {
+    public float hiz;
+    public GameObject donen;
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        donen.transform.Rotate(Vector3.up*Time.deltaTime*hiz);
+        
+    }
+}
