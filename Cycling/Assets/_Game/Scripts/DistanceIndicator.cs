a8a9bbd7947a13a613f9bc2e6c3ed61fd216d371
Egemen Can Güler <egemencanguler@gmail.com>
2018-06-30 13:20:17 +0300

    distance indicator

---
+++ Cycling/Assets/_Game/Scripts/DistanceIndicator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Kabuk;
+
+
+public class DistanceIndicator : MonoBehaviour 
+{
+
+	public RectTransform distanceBarBackground;
+	public Image distanceBar;
+
+
+	private void Update()
+	{
+		float height = distanceBar.rectTransform.sizeDelta.y;
+		float posY = distanceBar.fillAmount * height;
+		((RectTransform)transform).anchoredPosition = new Vector2(2, posY + 5);
+	}
+
+
+
+}
