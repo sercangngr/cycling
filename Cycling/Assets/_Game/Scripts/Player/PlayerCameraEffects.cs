fbabd6db41321a305270fbfe472c1d0fea5b37bc
Egemen Can Güler <egemencanguler@gmail.com>
2018-07-01 21:27:55 +0300

    item pickup effect

--- Cycling/Assets/_Game/Scripts/Player/PlayerCameraEffects.cs
+++ Cycling/Assets/_Game/Scripts/Player/PlayerCameraEffects.cs
@@ -9,6 +9,7 @@ public class PlayerCameraEffects : MonoBehaviour
 	public Color tunnelBlack = new Color(0, 0, 0, 0.7f);
 	public Color rainBlack = new Color(0, 0, 0, 0.3f);
 	public Color green = new Color(0, 1, 0, 0.2f);
+	public Color blue = new Color(0, 0, 1, 0.2f);
 
 	public Player player;
 	public Image effectImage;
@@ -21,23 +22,48 @@ public class PlayerCameraEffects : MonoBehaviour
 
 	private void Update()
 	{
-		if(player.state.hasShield)
-		{
-			gradiendEffectImage.color = green;
-			effectImage.color = transparent;
-		}else if(player.state.insideTunnel && !player.state.hasTorch)
+		if(player.state.insideTunnel && !player.state.hasTorch)
 		{
 			effectImage.color = tunnelBlack;
-			gradiendEffectImage.color = transparent;
 		}else if(player.state.insideRain)
 		{
 			effectImage.color = rainBlack;
-			gradiendEffectImage.color = transparent;
 		}else
 		{
-			gradiendEffectImage.color = transparent;
 			effectImage.color = transparent;
 		}
+	}
+
+	private void OnEnable()
+	{
+		GameState.EventNotify.RegisterCoroutine(OnNotification);
+		
+	}
+
+	private void OnDisable()
+	{
+		GameState.EventNotify.UnregisterCoroutine(OnNotification);
+	}
+
+	IEnumerator OnNotification(CollectableItem item)
+	{
+		if(item.type == CollectableItem.Type.SHIELD)
+		{
+			gradiendEffectImage.color = green;
+			yield return new WaitForSeconds(0.1f);
+			while(player.state.hasShield)
+			{
+				yield return null;
+			}
+			gradiendEffectImage.color = transparent;
+			
+		}else
+		{
+			gradiendEffectImage.color = blue;
+			yield return new WaitForSeconds(0.3f);
+			gradiendEffectImage.color = transparent;
+        }
+		
 	}
 
 
