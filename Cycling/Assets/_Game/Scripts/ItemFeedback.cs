fbc4467f90790c4c9e59eea7f5561c7349b679aa
MZI_ASUS\user <umutissi@gmail.com>
2018-08-01 13:11:27 +0300

    no message

--- Cycling/Assets/_Game/Scripts/ItemFeedback.cs
+++ Cycling/Assets/_Game/Scripts/ItemFeedback.cs
@@ -9,23 +9,33 @@ public class ItemFeedback : MonoBehaviour
 {
 	public Text numberText;
 	public Text contentText;
+    public RectTransform container;
 
     
 	public void Setup(CollectableItem item)
 	{
+        int number = 0;
 		if(item.energy > 0)
 		{
 			contentText.text = "Enerji";
-			numberText.text = "+" + ((int)item.energy);
+            number = ((int)item.energy);
 		}else if(item.score > 0)
 		{
 			contentText.text = "Puan";
-			numberText.text = "+" + ((int)item.score);
+            number = ((int)item.score);
 		}else if(item.time > 0)
 		{
 			contentText.text = "Zaman";
-			numberText.text = "+" + ((int)item.time);
+            number = ((int)item.time);
 		}
+
+        Vector3 pos = container.transform.localPosition;
+        if (Mathf.FloorToInt(number / 100) > 0)
+        {
+            pos.x = 164;
+        }
+        container.transform.localPosition = pos;
+        numberText.text = "+" + number;
 	}
 
 
